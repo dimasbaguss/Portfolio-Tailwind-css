5d78bdf37bd0b3b72b2b87d415851cc8c3c937e9
dimasbaguss <dimasbagusaputra97@gmail.com>
2022-08-04 22:09:56 +0700

    Menambahkan dark mode, tombol auto back to-top dan scroll smooth (beneran jalan)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,57 @@
 // Navbar Fixed
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
-}
+  const toTop = document.querySelector("#to-top");
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
+};
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Click di luar Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-mode-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    // Menambahkan local storage
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = 'light';
+  }
+});
+
+// Pindahkan posisi Toggle Dark Mode sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    // Jika check berada di Dark Mode jadikan true (kanan), dan sebaliknya
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
